69db2ea4e2d6bd773bbc096502380fd938177338
Ares <diego.amachiflores@gmail.com>
2023-10-09 16:40:35 -0500

    09/10/2023

--- clase-3/app.js
+++ clase-3/app.js
@@ -1,15 +1,67 @@
-/* eslint-disable no-unused-vars */
 const express = require('express')// require -----> commonJS
+const crypto = require('node:crypto')// native
 const movies = require('./movies.json')// require
-const app = express()
+const { validateMovie, validatePartialMovie } = require('./schemas/movies')
 
+const app = express()
+app.use(express.json())
 app.disable('x-powered-by')// disable header express
 
 app.get('/movies', (req, res) => {
+  const { genre } = req.query
+  if (genre) {
+    const filteredMovies = movies.filter(
+      movie => movie.genre.some(g => g.toLowerCase() === genre.toLowerCase())
+    )
+    return res.json({ filteredMovies })
+  }
   res.json(movies)
 })
+
 app.get('/movies/:id', (req, res) => { // path-to-regexp
   const { id } = req.params
+  const movie = movies.find(m => m.id === id)
+  if (movie) return res.json(movie)
+  res.status(404).json({ message: 'Movie not found' })
+})
+
+app.post('/movies', (req, res) => {
+  const result = validateMovie(req.body)
+
+  if (result.error) {
+    return res.status(400).json({ error: JSON.parse(result.error.message) })
+  }
+  const newMovie = {
+    id: crypto.randomUUID(),
+    ...result.data
+  }
+
+  movies.push(newMovie)
+
+  res.status(201).json(newMovie)
+})
+
+app.patch('/movies/:id', (req, res) => {
+  const result = validatePartialMovie(req.body)
+  if (!result.success) {
+    return res.status(400).json({ error: JSON.parse(result.error.message) })
+  }
+
+  const { id } = req.params
+  const movieIndex = movies.findIndex(movie => movie.id === id)
+
+  if (movieIndex === -1) {
+    return res.status(404).json({ message: 'Movie not found' })
+  }
+
+  const updateMovie = {
+    ...movies[movieIndex],
+    ...result.data
+  }
+
+  movies[movieIndex] = updateMovie
+
+  return res.json(updateMovie)
 })
 
 const PORT = process.env.PORT ?? 3000
